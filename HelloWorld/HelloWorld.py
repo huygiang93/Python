df3a6592947cabdd0f7dbfb4d11542559c434fcc
Huygiang <Huygiang1393@gmail.com>
2019-09-24 11:43:13 -0700

    count whitespace function

--- HelloWorld/HelloWorld.py
+++ HelloWorld/HelloWorld.py
@@ -1,18 +1,18 @@
-firstName = "Huy "
+firstName = "Huy"
 lastName = "Giang"
 fullName = firstName + " " + lastName
 
-def countws(name):
+def countws(string):
     whitespace = 0
-    for i in name:
-        if i == " ":
-            whitespace = whitespace + 1
-        else:
-            continue
-        return whitespace
+    for i in string:
+        if (i.isspace() == True):
+            whitespace+=1
+    return whitespace
+        
 
 
-print("Hello, %s"%firstName)
-print(len(fullName))
-print("number of whitespace:%d"%countws(fullName))
-print(len(fullName) - countws(fullName))
+string0 = "Hello, %s"%fullName
+
+print(string0)
+#print("length of string: %d"%len(string0))
+print("number of whitespace: %d"%countws(string0))
