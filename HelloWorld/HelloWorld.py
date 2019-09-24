7264087b59505524199d1fe2a9377ec8859b9878
Huygiang <Huygiang1393@gmail.com>
2019-09-24 13:11:29 -0700

    autoCap for first letters in first and last names

--- HelloWorld/HelloWorld.py
+++ HelloWorld/HelloWorld.py
@@ -1,6 +1,16 @@
-firstName = "Huy"
-lastName = "Giang"
-fullName = firstName + " " + lastName
+
+
+firstName =input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+
+def autoCap(string):
+    if string[0].islower():
+        return string[0].upper() + string[1:len(string)]
+
+autoCap(firstName)
+autoCap(lastName)
+fullName = autoCap(firstName) + " " + autoCap(lastName)
+
 
 def countws(string):
     whitespace = 0
@@ -14,5 +24,6 @@ def countws(string):
 string0 = "Hello, %s"%fullName
 
 print(string0)
-#print("length of string: %d"%len(string0))
+print("length of string: %d"%len(string0))
 print("number of whitespace: %d"%countws(string0))
+print("length w/o whitespace: %d"%(len(string0) - countws(string0)))
